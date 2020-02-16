b24aa63d202a7873e0995bb7f14d6740d00131bd
Jagjeet Singh <46004289+ijagjeet@users.noreply.github.com>
2020-02-16 19:24:53 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Remove image background with remove.bg API
+- Api link [Remove BG API](https://remove.bg)
+## Installation
+- Install python3 as per platform requirements
+- Install packages `pip install requirements`
+
+## Usage
+- Run command `python main.py`
---
+++ main.py
@@ -0,0 +1,56 @@
+import requests, random, os, sys
+
+
+class Croper:
+    def __init__(self, key):
+        self.head = {'X-Api-Key': key}
+
+    def cropbg(self, imgfile, bg_color):
+        req = requests.post('https://api.remove.bg/v1.0/removebg',
+                            files={'image_file': open(imgfile, 'rb')},
+                            data={'size': 'auto', 'bg_color': bg_color},
+                            headers=self.head,
+                            )
+
+        if req.status_code == requests.codes.ok:
+            rana = random.randrange(99999)
+            with open(str(rana) + '-rmbg.png', 'wb') as out:
+                out.write(req.content)
+            print(f"\n\033[92msuccess: {rana}-rmbg.png\033[0m")
+        else:
+            print(f"\n\033[91mError: {req.status_code} {req.text}\033[0m")
+
+    def cropbgurl(self, imgurl, bg_color):
+        req = requests.post('https://api.remove.bg/v1.0/removebg',
+                            data={'image_url': imgurl, 'size': 'auto', 'bg_color': bg_color},
+                            headers=self.head,
+                            )
+
+        if req.status_code == requests.codes.ok:
+            rana = random.randrange(99999)
+            with open(str(rana) + '-rmbg.png', 'wb') as out:
+                out.write(req.content)
+            print(f"\n\033[92msuccess: {rana}-rmbg.png\033[0m")
+        else:
+            print(f"\n\033[91mError: {req.status_code} {req.text}\033[0m")
+
+
+try:
+
+    os.system('clear')
+    imgna = input("Enter photo location or photo url: ")
+    bgcol = input(
+        "\n[info] (ex: red, blue) or can also use RGB code (ex: 81d4fa, fff) or leave it blank for transparent\nBlack color: ")
+    if not bgcol:
+        bgcol = None
+
+    crop = Croper('cD5ogD7V12dLAdkSfutJcHgT')
+    if 'http://' in imgna or 'https://' in imgna:
+        crop.cropbgurl(imgna, bgcol)
+    else:
+        crop.cropbg(imgna, bgcol)
+
+except KeyboardInterrupt:
+    sys.exit("EXIT: KEY INTERRUPT")
+except Exception as Err:
+    sys.exit(f"\n{Err}")
